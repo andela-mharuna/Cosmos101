2e18c1226cbb04a8d4ec94cb3b6d15174fe64a33
memuna haruna <memuna.haruna@andela.com>
2017-09-03 01:24:38 +0100

    Add controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+
+  root 'home#index'
+
+  get '/about' => 'home#about'
+
+  get '/questions/:id' => 'home#question'
+
+  post '/questions' => 'home#temp'
+
+  # post '/answers' => 'home#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
